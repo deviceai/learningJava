f386550c000e6034211f3302c3f0150c52e4d04a
deviceai <device.ai@gmail.com>
2019-11-14 15:51:19 +0200

    added someTests

---
+++ src/someTests/someTestsMain.java
@@ -0,0 +1,15 @@
+package someTests;
+
+public class someTestsMain {
+    public static void main(String[] args) {
+        int i = 10;
+        Long n = 20L;
+        float f = 10.0F;
+        //String s = (String) i+n++;  //not work
+
+        final boolean DEBUG = false;
+            //while(DEBUG){ //doesn't work
+                //System.out.println("debug statement here");  //unreachable statement
+           // }
+        }
+}
